6dc27f505de917e55902b6f68b68b5a780d78b3f
Tustin <Tustin@users.noreply.github.com>
2020-10-02 12:28:45 -0500

    scaffold

---
+++ shudder-trakt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shudder_trakt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
